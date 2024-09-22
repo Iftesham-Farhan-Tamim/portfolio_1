d28102746568e355aa9c248192070f03572cb4c9
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-09-22 14:38:58 +0600

    second commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import logo from "../assets/raviKumarLogo.webp"
+import { FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa'
+import { FaSquareXTwitter } from 'react-icons/fa6'
+import { SiCodeforces } from "react-icons/si";
+import { SiLeetcode } from "react-icons/si";
+
+function Navbar() {
+    return (
+        <nav className='flex items-center justify-between py-6'>
+            <div className='flex flex-shrink-0 items-center'>
+                <a href="/" aria-label='Home'>
+                    {/* <img src={logo} className='mx-2' width={150} height={33} alt="Logo" /> */}
+                </a>
+            </div>
+            <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+                <a href="https://linkedin.com/in/tamem-farhan"
+                    target='_black'
+                    rel='noopener noreferrer'
+                    aria-label='LinkedIn'>
+                    <FaLinkedin />
+                </a>
+                <a href="https://github.com/Iftesham-Farhan-Tamim?tab=repositories"
+                    target='_black'
+                    rel='noopener noreferrer'
+                    aria-label='GitHub'>
+                    <FaGithub />
+                </a>
+                <a href="https://codeforces.com/profile/Iftesham_Farhan_Tamim"
+                    target='_black'
+                    rel='noopener noreferrer'
+                    aria-label='Codeforces'>
+                    <SiCodeforces />
+                </a>
+                <a href="https://leetcode.com/u/iftesham_farhan_tamim/"
+                    target='_black'
+                    rel='noopener noreferrer'
+                    aria-label='LeetCode'>
+                    <SiLeetcode />
+                </a>     
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/constants/index.js
@@ -0,0 +1,107 @@
+import project1 from "../assets/projects/project-1.webp";
+import project2 from "../assets/projects/project-2.webp";
+import project3 from "../assets/projects/project-3.webp";
+import project4 from "../assets/projects/project-4.webp";
+import project5 from "../assets/projects/project-5.webp";
+
+export const HERO_CONTENT = `I am a passionate Front End developer with a knack for crafting robust and 
+scalable web applications. I have honed my skills in Front End technologies like React and Tailwind, as 
+well as Back End technologies like Spring boot, MySQL. My goal is to leverage my expertise to create 
+innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const RESUME = {
+  link: "https://drive.google.com/file/d/17LxTNbBMbohmgiMAEHi4NUkOyezAb4Hf/view?usp=drive_link",
+};
+
+export const ABOUT_TEXT = `I am a dedicated and versatile Front End developer with a passion for creating 
+efficient and user-friendly web applications. I have worked with a 
+variety of technologies, including React, Tailwind, MySQL. My journey in 
+web development began with a deep curiosity for how things work, and it has evolved into a career where I 
+continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy 
+solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, 
+exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Senior Full Stack Developer",
+    company: "Google Inc.",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and 
+    Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders 
+    to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Adobe",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Full Stack Developer",
+    company: "Facebook",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. 
+    Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional 
+    teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "Paypal",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. 
+    Managed databases and implemented data storage solutions using MongoDB. Worked closely with product 
+    managers to prioritize features and enhancements.`,
+    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Food & Restaurant Website",
+    image: project1,
+    link: "https://restaura-psi.vercel.app/",
+    description:
+      "A fully functional and responsive food and restaurant website with features like product listing & product details with users review.",
+    technologies: ["HTML", "CSS", "React JS", "Tailwind", "Framer-motion"],
+  },
+  {
+    title: "House Renovation Website",
+    image: project2,
+    link: "https://vastu-spaze-nine.vercel.app/",
+    description:
+      "A fully resposive website for showcasing services of house renovation, with features such as kitchen renovation, bathroom renovation & living room renovation services with users review.",
+    technologies: ["HTML", "CSS", "React", "Tailwind", "JavaScript"],
+  },
+  {
+    title: "Virtual Reality Software Website",
+    image: project3,
+    link: "https://vartual-ir.vercel.app/",
+    description:
+      "A fully functional and responsive website for vartual reality software company name Vartual IR, with provided features, AI assistance code review, pricing & users review.",
+    technologies: ["HTML", "CSS", "React", "Tailwind", "JavaScript"],
+  },
+  {
+    title: "Streamerzz the Ultimate Streaming Toolkit",
+    image: project4,
+    link: "https://streamerzz-two.vercel.app/",
+    description:
+      "A fully functional & responsive modern SeaS landing page for streamer application that optimize stream and provides real time analytics for gamers",
+    technologies: ["React", "Tailwind", "JavaScript", "Framer Motion"],
+  },
+  // {
+  //   title: "Portfolio Website",
+  //   image: project5,
+  //   description:
+  //     "A personal portfolio website showcasing projects, skills, and contact information.",
+  //   technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  // },
+];
+
+export const CONTACT = {
+  address: "Dhaka, Bangladesh",
+  phoneNo: "+8801552328744 ",
+  email: "tamim030116@gmail.com",
+};
